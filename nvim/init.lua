093db350c40b35ce72f8503aea1910301d92e5f5
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2024-05-03 22:00:50 +1000

    added treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,7 @@ vim.api.nvim_set_keymap("n", "x", '"_x', {noremap=true})
 vim.api.nvim_set_keymap("n", "X", '"_X', {noremap=true})
 vim.api.nvim_set_keymap("n", "<Del>", '"_x', {noremap=true})
 
+-- Lazy setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -25,17 +26,39 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    {
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     }
+  }
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- Treesitter
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
+
