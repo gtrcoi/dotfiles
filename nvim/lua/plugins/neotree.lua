b96af3786246b847db5aa0cf119f8a8bef74a7a4
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2024-09-27 19:04:44 +1000

    remove neotree config function

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -7,7 +7,5 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  config = function()
-    vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left toggle<CR>', {})
-  end
+  opts = {}
 }
