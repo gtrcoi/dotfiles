dd28a02e3026769e15170739ca852096228e6586
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2024-10-02 22:14:51 +1000

    init

---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {}
+  },
+  {
+    'maxmx03/solarized.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      variant = 'summer',
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+}
