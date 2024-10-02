d4f2942379b9a8f65a94e77980f8643551a445ee
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2024-10-02 22:11:49 +1000

    changed theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
-      theme = "palenight"
+      -- theme = "palenight"
+      theme = "auto"
     }
   },
   -- Available components:
