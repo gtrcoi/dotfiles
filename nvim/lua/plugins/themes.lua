6b5e775afd4f3485cd1b0c3f84988cf8508a457f
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2024-10-22 07:30:14 +1000

    add themes

---
+++ nvim/lua/plugins/themes.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      -- transparent_mode = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "maxmx03/solarized.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      variant = "summer",
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
