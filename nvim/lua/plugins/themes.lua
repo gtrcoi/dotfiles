21e8149e71911f5020b0bd393e4d5325e39ce89e
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2024-12-05 13:32:42 +1000

    add lushwal and misc

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -30,9 +30,17 @@ return {
     },
   },
   {
+    "oncomouse/lushwal.nvim",
+    cmd = { "LushwalCompile" },
+    dependencies = {
+      { "rktjmp/lush.nvim" },
+      { "rktjmp/shipwright.nvim" },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "lushwal",
     },
   },
 }
