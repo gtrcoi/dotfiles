d5391eba3cca638b84ba69b114584cf9f3b5ea47
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2026-03-02 00:52:55 +1000

    feat(wofi): work on screenshots

--- roles/wofi/files/wofi-screenshots.sh
+++ roles/wofi/files/wofi-screenshots.sh
@@ -6,7 +6,17 @@ SCREENSHOT_DIR="$HOME/Pictures/Screenshots"
 # 1. Get the files
 # 2. Add the img: prefix for Wofi
 # 3. Pipe to wofi in dmenu mode
-selected=$(ls "$SCREENSHOT_DIR" | sed "s|^|img:$SCREENSHOT_DIR/|" | wofi --dmenu --allow-images) #--conf=$HOME/.config/wofi/config)
+selected=$(
+  ls "$SCREENSHOT_DIR" |
+    sed "s|^|img:$SCREENSHOT_DIR/|" |
+    wofi --dmenu \
+      --cache-file /dev/null \
+      --allow-images \
+      --columns 3 \
+      --define "image-size=250x141" \
+      --conf ~/.config/wofi/wallpaper \
+      --sort-order=default
+) #--conf=$HOME/.config/wofi/config)
 
 # Optional: Open the selected file with your image viewer (e.g., sxiv, imv, or feh)
 if [ -n "$selected" ]; then
