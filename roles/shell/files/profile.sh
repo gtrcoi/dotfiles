572067153edbb3eda61aa76a976c2164a177f8a3
gtrcoi <24573615+gtrcoi@users.noreply.github.com>
2026-03-18 00:22:58 +1000

    feat(shell): clean up profile

--- roles/shell/files/profile.sh
+++ roles/shell/files/profile.sh
@@ -1,13 +1,12 @@
+# Export environment.d
+eval $(/lib/systemd/user-environment-generators/30-systemd-environment-d-generator | sed 's/^/export /')
+
 # default programs
 export TERM=alacritty
 export TERMINAL=alacritty
+export BROWSER="firefox"
 
-# add scripts to path
-export PATH="$XDG_CONFIG_HOME/scripts:$PATH"
-
-##############
 # EDITOR env
-##############
 if [ -x "/usr/bin/nvim" ]; then
   export EDITOR="nvim"
   export MANPAGER='nvim +Man!'
@@ -16,45 +15,28 @@ elif [ -d "$HOME/.var/app/io.neovim.nvim" ]; then
   export MANPAGER="less"
 fi
 
-#############
-# NVM setup
-#############
-[ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.local/share/nvm"
-if [ -e /usr/share/nvm/init-nvm.sh ]; then
-  source /usr/share/nvm/nvm.sh
-  source /usr/share/nvm/bash_completion
-  source /usr/share/nvm/install-nvm-exec
-fi
+# PATH additions
+[ -d "$HOME/bin" ] && PATH="$HOME/bin:$PATH"
+[ -d "$HOME/.local/bin" ] && PATH="$HOME/.local/bin:$PATH"
+[ -d "$XDG_CONFIG_HOME/scripts" ] && PATH="$XDG_CONFIG_HOME/scripts:$PATH"
 
 # dircolors
-if [ -e ~/.dircolors ]; then
-  eval 'dircolors ~/.dircolors'
+[ -e "$HOME/.dircolors" ] && eval 'dircolors ~/.dircolors'
+
+# NVM setup
+[ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.local/share/nvm"
+if [ -e /usr/share/nvm/init-nvm.sh ]; then
+  . /usr/share/nvm/nvm.sh
+  . /usr/share/nvm/bash_completion
+  . /usr/share/nvm/install-nvm-exec
 fi
 
+# picom
 _display_type=$(loginctl show-session self -p Type | awk -F= '{print $2}')
 if [ -e /usr/bin/picom ] && [ "$_display_type" = "x11" ] && ! pgrep -x "picom"; then
   picom -b --config $XDG_CONFIG_HOME/picom/picom.conf
 fi
-
-restore_wallpaper() {
-  _display_type=$(loginctl show-session self -p Type | awk -F= '{print $2}')
-  # Wayland
-  if [ "$_display_type" = "wayland" ] && command -v swww >/dev/null; then
-    swww-daemon >/dev/null 2>&1 &
-    swww restore
-
-  # X11
-  elif [ "$_display_type" = "x11" ]; then
-    if command -v nitrogen >/dev/null; then
-      nitrogen --restore &
-    elif [ -f "$HOME/.fehbg" ]; then
-      sh "$HOME/.fehbg"
-    fi
-  fi
-}
-
 unset _display_type
-########################
-# Export environment.d
-########################
-eval $(/lib/systemd/user-environment-generators/30-systemd-environment-d-generator | sed 's/^/export /')
+
+# Restore wallpaper
+[ -f "$XDG_CONFIG_HOME/shell/wallpaper.sh"] && . "$XDG_CONFIG_HOME/shell/wallpaper.sh"
